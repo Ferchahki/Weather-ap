c97613d62f189df3d8de0e319c602b4ea18ae345
Ferchahki <alawiabdo000@gmail.com>
2023-08-20 21:09:06 +0100

    Final

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "weather-primary": "#55efc4",
+        "weather-secondary": "#feca57",
+      },
+      fontFamily: {
+        Roboto: ["Roboto, sans-serif"],
+      },
+      container: {
+        padding: "2rem",
+        center: true,
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+      },
+    },
+  },
+  plugins: [],
+}
+
